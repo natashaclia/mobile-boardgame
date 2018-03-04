490f523cb9cbed9d124fd46dd0716dcdae8c2c2e
natashaclia <32930065+natashaclia@users.noreply.github.com>
2018-03-04 18:45:20 +0000

    Add files via upload
    draft unfinished work for mobile board game

---
+++ Mobile Game DRAFT WORK/DiceRoll.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoll : MonoBehaviour {
+
+    //GameObject diceImage;
+    //Animator anim;
+
+    //public Sprite[] dice;
+    //public int rollNum;
+
+    //private bool isRolling;
+    //private bool diceRolled;
+
+    public Button dice;
+    public Animator anim;
+    public Canvas can;
+    public float animTime = 3.0f;
+    public bool animated = false;
+
+    float currentTime = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+        //nim = GetComponent<Animator>();
+        // finds the object named "Dice" 
+        //diceImage = GameObject.Find("Dice");
+
+        //diceRolled = false;
+        //isRolling = false;
+
+        dice = GetComponent<Button>();
+        anim.enabled = false;
+        can.enabled = true;
+        currentTime = Time.time;
+	}
+
+    // when button pressed
+    public void Press()
+    {
+        dice.enabled = true;
+        anim.enabled = true;
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+
+        //if (Input.touchCount == 1)
+        //{
+        //  diceImage.GetComponent("Rolling");
+        //anim.Play("Rolling");
+        //isRolling = true;
+        //}
+
+    }
+
+    public void TimeElapsed()
+    {
+        if (Time.time == animTime)
+        {
+            dice.enabled = false;
+            anim.StopRecording();
+        }
+    }
+}
